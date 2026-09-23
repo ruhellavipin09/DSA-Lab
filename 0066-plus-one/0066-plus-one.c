67ccc9de202cc05999bc9b63e83494842584a114
VIPIN RUHELLA <ruhellavipin09@gmail.com>
2026-09-23 05:11:54 -0400

    Time: 0 ms (100%), Space: 10.1 MB (32.11%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.c
@@ -0,0 +1,23 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    int i;
+    for(i=digitsSize-1;i>=0;i--)
+    {
+        if(digits[i]<9)
+        {
+            digits[i]++;
+            *returnSize=digitsSize;
+            return digits;
+        }
+        digits[i]=0;
+    }
+    int *ans;
+    ans=(int*)malloc((digitsSize+1)*sizeof(int));
+    ans[0]=1;
+    for(i=1;i<=digitsSize;i++)
+    ans[i]=0;
+    *returnSize=digitsSize+1;
+    return ans;
+}
